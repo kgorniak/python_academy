e334b49538ab84e6501dffa82c2d8a97fb1fbc6d
kgorniak <117675151+kgorniak@users.noreply.github.com>
2022-12-22 11:18:44 +0100

    api tests, AoC d5

--- Advent_Of_Code_2022/day5.py
+++ Advent_Of_Code_2022/day5.py
@@ -1,3 +1,5 @@
+import copy
+
 # task description: https://adventofcode.com/2022/day/5
 
 with open("day5_input.txt", "r") as file:
@@ -24,19 +26,19 @@ for line in all_lines_list[:8]:
 # print(lines_all)
 
 # stworzenie stosów w liście
-stacks_list = []
+stacks_list_master = []
 for counter in range(len(lines_all) + 1):
     stack = []
     for item in lines_all:
         stack.append(item[counter])
     stack.reverse()
-    stacks_list.append(stack)
+    stacks_list_master.append(stack)
 # print(stacks_list)
 
 # usunięcie pustych ('') miejsc na stosach
-for item in stacks_list:
+for item in stacks_list_master:
     item[:] = [letter for letter in item if letter != '']
-print(stacks_list)
+# print(stacks_list_master)
 
 # wrzucenie instrukcji do listy
 instructions_list = []
@@ -47,19 +49,35 @@ for line in all_lines_list[10:]:
 
 # part 1
 # użycie instrukcji (przekładanie skrzyń na stosach - pojedynczo)
+stacks_list1 = copy.deepcopy(stacks_list_master)
 for instruction in instructions_list:
     how_many_to_move = int(instruction[1])
     from_stack = int(instruction[3]) - 1
     to_stack = int(instruction[5]) - 1
     for i in range(how_many_to_move):
-        moving_item = stacks_list[from_stack][-1]
-        del stacks_list[from_stack][-1]
-        stacks_list[to_stack].append(moving_item)
+        moving_item = stacks_list1[from_stack][-1]
+        del stacks_list1[from_stack][-1]
+        stacks_list1[to_stack].append(moving_item)
 
 # wyciągnięcie wyniku końcowego
-text_answer = ""
-for stack in stacks_list:
-    text_answer += stack[-1].rstrip("]").lstrip("[")
-print(f"Answer part 1 : {text_answer}\n")
+text_answer1 = ""
+for stack in stacks_list1:
+    text_answer1 += stack[-1].rstrip("]").lstrip("[")
+print(f"Answer part 1 : {text_answer1}\n")
 
 # part 2
+# użycie instrukcji (przekładanie skrzyń na stosach - po kilka na raz)
+stacks_list2 = copy.deepcopy(stacks_list_master)
+for instruction in instructions_list:
+    how_many_to_move = int(instruction[1])
+    from_stack = int(instruction[3]) - 1
+    to_stack = int(instruction[5]) - 1
+    moving_items = stacks_list2[from_stack][-how_many_to_move:]
+    del stacks_list2[from_stack][-how_many_to_move:]
+    for item in moving_items:
+        stacks_list2[to_stack].append(item)
+
+text_answer2 = ""
+for stack in stacks_list2:
+    text_answer2 += stack[-1].rstrip("]").lstrip("[")
+print(f"Answer part 2 : {text_answer2}\n")
--- api_tests/pokemon_api_tests.py
+++ api_tests/pokemon_api_tests.py
@@ -20,6 +20,19 @@ class PokemonTests(unittest.TestCase):
         self.assertTrue(1000 > roundtrip)
         self.assertLess(size, 100)
 
+    def test_get_pokemons_with_offset(self):
+        params = {
+            "offset": 20,
+            "limit": 10
+        }
+        response = requests.get(self.url + self.pokemon_endpoint, params=params)
+        response_body = response.json()
+
+        self.assertTrue(response_body)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(10, len(response_body["results"]))     # check limit param
+        self.assertRegex(response_body["results"][0]['url'], ".*\/21\/")    # check offset param
+
 
 if __name__ == '__main__':
     unittest.main()
