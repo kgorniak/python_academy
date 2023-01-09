9f87d6b58512e30baafd5af2f8fc8de1a415e5b1
kgorniak <117675151+kgorniak@users.noreply.github.com>
2023-01-09 17:51:15 +0100

    first ui tests

--- api_tests/gorest_api_handler.py
+++ api_tests/gorest_api_handler.py
@@ -1,13 +1,38 @@
 import requests
+import os
 
 
 class APIHandler:
-    url = "https://gorest.co.in/public/v2"
+    url = "https://gorest.co.in/public"
     user_endpoint = "/users"
+    api_version_dict = {
+        "v1": "/v1",
+        "v2": "/v2",
+        "public": "/public-api"
+    }
 
-    def create_user(self, user_data):
-        res = requests.post(f"{self.url}{self.user_endpoint}")
-        return res
+    def __init__(self, token="", api_ver="v2"):
+        self.url = f"{self.url}{self.api_version_dict[api_ver]}"
+        if not token:
+            token = os.environ["GOREST_TOKEN"]
+        self.headers = {
+            "Authorization": f"Bearer {token}"
+        }
+
+    def create_user(self, user_data, expected_status_code=201):
+        res = requests.post(f"{self.url}{self.user_endpoint}", json=user_data, headers=self.headers)
+        if expected_status_code:
+            assert expected_status_code == res.status_code
+        return res.json()
 
     def get_user(self, user_id):
-        res = requests.get(f"{self.url}{self.user_endpoint}/{user_id}")
+        res = requests.get(f"{self.url}{self.user_endpoint}/{user_id}", headers=self.headers)
+        # if expected_status_code:
+        #     assert expected_status_code == res.status_code
+        return res.json()
+
+    def update_user(self, user_id, params):
+        requests.put(f"{self.url}{self.user_endpoint}/{user_id}", params=params, headers=self.headers)
+
+    def delete_user(self, user_id):
+        requests.delete(f"{self.url}{self.user_endpoint}/{user_id}", headers=self.headers)
--- python_tests/test_gorest.py
+++ python_tests/test_gorest.py
@@ -1,47 +1,50 @@
 import unittest
-from faker import Faker
+
 import requests
+from faker import Faker
 from api_tests.gorest_api_handler import APIHandler
 
 
 class TestGorest(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.api_handler = APIHandler
+        self.api_handler = APIHandler("0cc9c693995f6c1c628b5c00bd47c4b46dffb1acdbedc5f0a867ff70a11190b5")
+        self.faker = Faker()
 
-    def test_Create_user(self):
-        url = "https://gorest.co.in/public/v2/users"
-        faker = Faker()
-        fake_email = faker.free_email()
-        print(fake_email)
-        header = {
-            "Authorization": "Bearer 0cc9c693995f6c1c628b5c00bd47c4b46dffb1acdbedc5f0a867ff70a11190b5"
-        }
-        params = {
-            "name": "Tenali Ramakrishna",
-             "gender": "male",
-             "email": fake_email,
-             "status": "active"
+    def test_create_modify_and_delete_user(self):
+
+        # create user
+        new_user = {
+            "name": self.faker.name(),
+            "gender": "male",
+            "email": self.faker.free_email(),
+            "status": "active"
         }
 
-        create_user = requests.post(url, headers=header, params=params)
-        # print(create_user.status_code)
-        created_user_body = create_user.json()
-        user_id = created_user_body["id"]
-        print(user_id)
-        self.assertTrue(created_user_body["id"])
+        create_user_body = self.api_handler.create_user(new_user)
+        self.user_id = create_user_body["id"]
+        self.assertTrue(self.user_id)
 
-        params2 = {
-            "name": "Adam Test"
+        # get user
+        user_body = self.api_handler.get_user(self.user_id)
+        self.assertTrue(user_body)
+
+        # update user
+        data_to_update = {
+            "name": "Adam Test",
+            "gender": "female"
         }
-        update_url = f"{url}/{user_id}"
-        print(update_url)
-        requests.put(f"{url}/{user_id}", params=params2)
-        updated_user = requests.get(f"{url}/{user_id}").json()
-        print(updated_user)
-        print(updated_user["name"])
-        print(params2["name"])
-        # self.assertEqual(updated_user["name"], params2["name"])
+
+        self.api_handler.update_user(user_id=self.user_id, params=data_to_update)
+        updated_user_body = self.api_handler.get_user(self.user_id)
+        self.assertEqual(data_to_update["name"], updated_user_body["name"])
+        self.assertEqual(data_to_update["gender"], updated_user_body["gender"])
+
+        # delete user
+        self.api_handler.delete_user(user_id=self.user_id)
+        res = requests.get(f"{self.api_handler.url}{self.api_handler.user_endpoint}/{self.user_id}",
+                           headers=self.api_handler.headers)
+        self.assertEqual(404, res.status_code)
 
 
 if __name__ == '__main__':
